cf732fdb9d5bec3f1adf4bfd52b86287a452b2f6
Brewski <andrewweston@me.com>
2025-09-29 16:46:40 +0000

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -15,7 +15,7 @@ export function initViewer(root){
   renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   renderer.setSize(root.clientWidth, root.clientHeight);
-  renderer.setClearColor(0xffffff, 1); // white background
+  renderer.setClearColor(0x808080, 1); // white background
   root.appendChild(renderer.domElement);
 
   controls = new OrbitControls(camera, renderer.domElement);
